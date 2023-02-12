3dba1d2598ac9449b6cc37fcc1ab78a511f37d96
sutepjantawee <bananammm0001@gmail.com>
2023-02-12 19:32:37 +0700

    add Carousel

--- pages/component/title.tsx
+++ pages/component/title.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Text, Row,Col, } from "@nextui-org/react";
+import { Text, Row,Col, Container, Grid, Spacer, } from "@nextui-org/react";
 import Image from 'next/image'
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 export default function title() {
   return (
     <div> 
 
-      <Row  justify="center" align="center" css={{ fontfamily: 'Montserrat', pt:178 }} >
+      <Row  justify="center" align="center" css={{ fontfamily: 'Montserrat', pt:178,}} >
           <div >
           <Text weight="bold" css={{color: '#FFFFFF', fontSize: '6rem', }} >
           LOREM 
@@ -35,24 +37,69 @@ export default function title() {
             
         </div>         
       </Row>
-     
-      <Row justify="center">
-        <Text weight="bold" css={{ textAlign:'center', pb:64, fontSize: '3.75rem', pt:200}}>LOREM IPSUM </Text>
-        <Text weight="bold" css={{ color: '#D9EF0C', pl:14, textAlign:'center', pb:64, fontSize: '3.75rem', pt:200}}>LOREM</Text>
-      </Row>
       
-        <Row justify="center">
-            <Image src="/Ellipse.png" width={200} height={200} className="rounded-full " alt={""} />      
-        </Row>         
-        <Text weight="bold"  css={{pt:61, textAlign:'center', fontSize: '1.75rem',}}>Lorem Ipsum is simply</Text>
-        <Text css={{pt:20,pb:225,textAlign:'center', fontSize: '1.125rem', }}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-          <br/>
-          been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took 
-          <br/>
-          a galley of type and scrambled it to make a type specimen book.
-       </Text>
-      
+      <Spacer y={10} />
+
+       <Carousel >
+          <div>
+              <Row justify="center" >
+                <Text weight="bold" css={{ textAlign:'center', fontSize: '3.75rem', }}>LOREM IPSUM </Text>
+                <Text weight="bold" css={{ color: '#D9EF0C', pl:14, textAlign:'center',  fontSize: '3.75rem', }}>LOREM</Text>
+              </Row>
+              <Grid.Container justify="center">
+                <Row css={{width:200,height:200,mt:60}}>
+                    <Image src="/Ellipse.png" width={200} height={200} className="rounded-full " alt={""} />      
+                </Row>
+                </Grid.Container>         
+                <Text weight="bold"  css={{pt:61, textAlign:'center', fontSize: '1.75rem',}}>Lorem Ipsum is simply</Text>
+                <Text css={{pt:20,textAlign:'center', fontSize: '1.125rem', mb:90,}}>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                  <br/>
+                  been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took 
+                  <br/>
+                  a galley of type and scrambled it to make a type specimen book.
+              </Text>
+          </div>
+          <div>
+              <Row justify="center" >
+                <Text weight="bold" css={{ textAlign:'center', fontSize: '3.75rem', }}>LOREM IPSUM </Text>
+                <Text weight="bold" css={{ color: '#D9EF0C', pl:14, textAlign:'center',  fontSize: '3.75rem', }}>LOREM</Text>
+              </Row>
+              <Grid.Container justify="center">
+                <Row css={{width:200,height:200,mt:60}}>
+                    <Image src="/Ellipse.png" width={200} height={200} className="rounded-full " alt={""} />      
+                </Row>
+                </Grid.Container>         
+                <Text weight="bold"  css={{pt:61, textAlign:'center', fontSize: '1.75rem',}}>Lorem Ipsum is simply</Text>
+                <Text css={{pt:20,textAlign:'center', fontSize: '1.125rem', mb:90,}}>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                  <br/>
+                  been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took 
+                  <br/>
+                  a galley of type and scrambled it to make a type specimen book.
+              </Text>
+          </div>
+          <div>
+              <Row justify="center" >
+                <Text weight="bold" css={{ textAlign:'center', fontSize: '3.75rem', }}>LOREM IPSUM </Text>
+                <Text weight="bold" css={{ color: '#D9EF0C', pl:14, textAlign:'center',  fontSize: '3.75rem', }}>LOREM</Text>
+              </Row>
+              <Grid.Container justify="center">
+                <Row css={{width:200,height:200,mt:60}}>
+                    <Image src="/Ellipse.png" width={200} height={200} className="rounded-full " alt={""} />      
+                </Row>
+                </Grid.Container>         
+                <Text weight="bold"  css={{pt:61, textAlign:'center', fontSize: '1.75rem',}}>Lorem Ipsum is simply</Text>
+                <Text css={{pt:20,textAlign:'center', fontSize: '1.125rem', mb:90,}}>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                  <br/>
+                  been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took 
+                  <br/>
+                  a galley of type and scrambled it to make a type specimen book.
+              </Text>
+          </div>        
+        </Carousel>
+        <Spacer y={5} />
     </div>
   
    
